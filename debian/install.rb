3e94948555d4aa6bf6331eef3f991dd1eac99ed4
tach <tach@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2003-09-11 08:16:53 +0000

    Add debian package files
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@171 684b846f-27f2-41ea-b66a-77a1bafa84c1

---
+++ debian/install.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require "rbconfig"
+require "ftools"
+include Config
+
+DSTPATH = ENV['DESTDIR'] + CONFIG["rubylibdir"]
+File.makedirs(DSTPATH)
+
+def join( *arg )
+  File.join( *arg )
+end
+
+def base( name )
+  File.basename( name )
+end
+
+begin
+  Dir.glob('hiki/**/*.rb').each do | name |
+    File.makedirs(join(DSTPATH, File.dirname(name)))
+    File.install(name, join(DSTPATH, name), 0644, true )
+  end
+
+  puts "install succeed!"
+
+rescue 
+  puts "install failed!"
+  puts $!
+end
