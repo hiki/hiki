b84dc77f6645e5b5735b76b064cb3c9007321864
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2004-05-12 01:42:40 +0000

    revise for $SAFE=1
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@251 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- misc/plugin/attach/attach.rb
+++ misc/plugin/attach/attach.rb
@@ -1,4 +1,4 @@
-# $Id: attach.rb,v 1.6 2004-04-18 07:22:51 fdiary Exp $
+# $Id: attach.rb,v 1.7 2004-05-12 01:42:40 fdiary Exp $
 # Copyright (C) 2003 TAKEUCHI Hitoshi <hitoshi@namaraii.com>
 #
 # thanks to Kazuhiko, Masao Mutoh, SHIMADA Mitsunobu, Yoshimi, ¤ê¤¿
@@ -132,7 +132,7 @@ def attach_download
   mime_type   = mime_types[extname]
 
   print "Content-Type: #{mime_type}\n"
-  print "Last-Modified: #{CGI::rfc1123_date(File.mtime(attach_file))}\n"
+  print "Last-Modified: #{CGI::rfc1123_date(File.mtime(attach_file.untaint))}\n"
   print %Q!Content-Disposition: filename="#{file_name.to_sjis}"\n\n!
   print open(attach_file.untaint, "rb").read
   nil
