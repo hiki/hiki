a56ef77ce687f3525ce0b80869c080dd1ac236d5
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2004-06-26 14:19:03 +0000

    correct major version number
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@277 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- hiki/config.rb
+++ hiki/config.rb
@@ -1,9 +1,9 @@
-# $Id: config.rb,v 1.1 2004-06-26 14:12:28 fdiary Exp $
+# $Id: config.rb,v 1.2 2004-06-26 14:19:03 fdiary Exp $
 # Copyright (C) 2004 Kazuhiko <kazuhiko@fdiary.net>
 #
 # TADA Tadashi <sho@spc.gr.jp> holds the copyright of Config class.
 
-HIKI_VERSION  = '0.8-devel-20040626'
+HIKI_VERSION  = '0.7-devel-20040626'
 
 module Hiki
   PATH  = "#{File::dirname(File::dirname(__FILE__))}"
