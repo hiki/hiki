03ae5073e268f2050a8ded027b99f79b5e75e0f7
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-01-29 05:06:53 +0000

    add ja/
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@406 684b846f-27f2-41ea-b66a-77a1bafa84c1

---
+++ misc/plugin/ja/math.rb
@@ -0,0 +1,11 @@
+def label_math_latex_ptsize; 'フォントサイズ (pt)'; end
+def label_math_latex_documentclass; 'ドキュメントクラス'; end
+def label_math_latex_preamble; 'プリアンブル'; end
+def label_math_latex_log; 'ログの保持'; end
+def label_math_latex_log_description; 'タイプセット・コマンド実行時の標準出力・
+標準エラー出力のログを削除しない．'; end
+def label_math_latex_cache_clear; 'キャッシュのクリア'; end
+def label_math_latex_cache_clear_description; 'チェックして OK すると，現時点で保持されている画像のキャッシュを削除する．'; end
+def label_math_latex_latex; 'latex コマンド'; end
+def label_math_latex_dvips; 'dvips コマンド'; end
+def label_math_latex_convert; 'convert コマンド'; end
