ec8060cecc21ca319fddbcd062d79087a830308e
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-03-01 07:58:13 +0000

    * plugin/en/00default.rb, messages/en.rb: メッセージの修正 by         Steve Brumbaugh
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@432 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- messages/en.rb
+++ messages/en.rb
@@ -1,4 +1,4 @@
-# $Id: en.rb,v 1.7 2005-02-14 07:51:09 fdiary Exp $
+# $Id: en.rb,v 1.8 2005-03-01 07:58:13 fdiary Exp $
 # en.rb
 #
 # Copyright (C) 2003 Masao Mutoh <mutoh@highway.ne.jp>
@@ -19,60 +19,60 @@ module Hiki
     def msg_search; 'Search' end
     def msg_admin; 'Admin' end
     def msg_search_result; 'Search Results' end
-    def msg_search_hits; '\'%1$s\': %3$d page(s) were found in %2$d pages.' end
-    def msg_search_not_found; '\'%s\'was not found.' end
-    def msg_search_comment; 'Search from whole this site. Ignore Upper/Lowere cases. Hiki returns pages that contain all the words in your query' end
+    def msg_search_hits; '%3$d page(s) containing the phrase \'%1$s\' were found in a set of %2$d pages.' end
+    def msg_search_not_found; 'No pages containing the phrase \'%s\' were found.' end
+    def msg_search_comment; 'Searches all pages, ignore case, and return pages that contain all words in your query.' end
     def msg_frontpage; 'Top' end
     def msg_hitory; 'History' end
-    def msg_index; 'Indexes' end
+    def msg_index; 'Index' end
     def msg_recent_changes; 'Changes' end
     def msg_newpage; 'New' end
-    def msg_no_recent; '<P>There are no data.</P>' end
-    def msg_thanks; 'Thanks.' end
-    def msg_save_conflict; 'Conflict your updating. Your changes wasn\'t saved. Copy your changes to your editor yourself. And reload the pages and edit it again.' end
+    def msg_no_recent; '<P>There is no data.</P>' end
+    def msg_thanks; 'Thank you for your update.' end
+    def msg_save_conflict; 'There is a conflict with your updates.  Copy the content below to a text editor and edit the page after referring to the latest version.' end
     def msg_time_format; "%Y-%m-%d #DAY# %H:%M:%S" end
     def msg_date_format; "%Y-%m-%d " end
     def msg_day; %w(Sun Mon Tue Wed Thr Fri Sat) end
-    def msg_preview; 'Confirm the result below, and save it with clicking Save button if there are no problem' end
-    def msg_mail_on; 'Send updated-mail' end
-    def msg_mail_off; 'Doesn\'t send updated-mail' end
+    def msg_preview; 'Confirm the content below.  If there are no problems, save it with the save button.' end
+    def msg_mail_on; 'Send update e-mails' end
+    def msg_mail_off; 'Do not send update e-mails' end
     def msg_use; 'Use' end
-    def msg_unuse; 'Doesn\'t use' end
-    def msg_password_title; 'Admin\'s Password' end
-    def msg_password_enter; 'Input Admin\'s password.' end
+    def msg_unuse; 'Don\'t use' end
+    def msg_password_title; 'Admin password' end
+    def msg_password_enter; 'Enter admin password' end
     def msg_password; 'Password' end
     def msg_ok; 'OK' end
-    def msg_invalid_password; 'Password isn\'t correct. Your changes has not saved yet.' end
-    def msg_save_config; 'Saved your changes' end
-    def msg_freeze; 'This page is freezed. You need Admin\'s password for saving this page.' end
-    def msg_freeze_mark; '[Freeze]' end
-    def msg_already_exist; 'The page has already been existed.' end
-    def msg_page_not_exist; 'The page is not exist. Please create it yourself:-)' end
-    def msg_invalid_filename(s); "Include invalid character, or is over max length(#{s}byte). Change the page name." end
+    def msg_invalid_password; 'Your password is not correct.  Your changes have not yet been saved.' end
+    def msg_save_config; 'Your configuration changes have been saved.' end
+    def msg_freeze; 'This page is frozen.  You will need an admin password to change this page.' end
+    def msg_freeze_mark; '[Frozen]' end
+    def msg_already_exist; 'That page already exists.' end
+    def msg_page_not_exist; 'This page does not exist.  Feel free to create it yourself :-)' end
+    def msg_invalid_filename(s); "The page name contains an invalid character or is over the maximum length of #{s} bytes.  Please fix the page name." end
     def msg_delete; 'Deleted' end
-    def msg_delete_page; 'The page deleted.' end
-    def msg_follow_link; 'Click the anchor below to show your page: ' end
-    def msg_match_title; '[matched in title]' end
-    def msg_match_keyword; '[matched in keyword]' end
-    def msg_duplicate_page_title; 'The page title has already been existed.' end
-    def msg_missing_anchor_title; 'Create new %s and edit.' end
+    def msg_delete_page; 'The page has been deleted.' end
+    def msg_follow_link; 'Click the following link to view your page: ' end
+    def msg_match_title; '(matched in title)' end
+    def msg_match_keyword; '(matched in keyword)' end
+    def msg_duplicate_page_title; 'That page title already exists.' end
+    def msg_missing_anchor_title; 'Create and edit the page %s.' end
     # (config)
-    def msg_config; 'Hiki Configurations'; end
+    def msg_config; 'Hiki Configuration'; end
     # (diff)
-    def msg_diff_add; 'Added words are showed as <ins class="added">here</ins>.'; end
-    def msg_diff_del; 'Removed words are showed as <del class="deleted">here</del>.'; end
+    def msg_diff_add; 'Added parts are displayed <ins class="added">like this</ins>.'; end
+    def msg_diff_del; 'Deleted parts are displayed <del class="deleted">like this</del>.'; end
     # (edit)
     def msg_title; 'Title'; end
-    def msg_keyword_form; 'Keyword (one keyword per line)'; end
+    def msg_keyword_form; 'Keyword(s) (one keyword per line)'; end
     def msg_freeze_checkbox; 'Freeze the current page.'; end
     def msg_password_form; 'Password'; end
     def msg_preview_button; 'Preview'; end
     def msg_save; 'Save'; end
-    def msg_latest; 'Refer recent version'; end
-    def msg_rules; %Q|See <a href="#{@cgi_name}?TextFormattingRules">TextFormattingRules</a> if you need.|; end
+    def msg_latest; 'Latest version'; end
+    def msg_rules; %Q|See <a href="#{@cgi_name}?EnglishTextFormattingRules">EnglishTextFormattingRules</a> for formatting help.|; end
     # (view)
-    def msg_last_modified; 'Last Modified'; end
-    def msg_keyword; 'Keyword'; end
+    def msg_last_modified; 'Last modified'; end
+    def msg_keyword; 'Keyword(s)'; end
     def msg_reference; 'References'; end
   end
 end
--- plugin/en/00default.rb
+++ plugin/en/00default.rb
@@ -7,20 +7,20 @@
 add_conf_proc( 'default', 'Basic preferences' ) do
   saveconf_default
   <<-HTML
-      <h3 class="subtitle">Site names</h3>
-      <p>Set your site name. This appeared as title element.</p>
+      <h3 class="subtitle">Site name</h3>
+      <p>Enter the name of your site.  This will appear in page titles.</p>
       <p><input name="site_name" value="#{CGI::escapeHTML(@conf.site_name)}" size="40"></p>
       <h3 class="subtitle">Author</h3>
-      <p>Set your name.</p>
+      <p>Enter your name.</p>
       <p><input name="author_name" value="#{CGI::escapeHTML(@conf.author_name)}" size="40"></p>
-      <h3 class="subtitle">E-Mail address</h3>
-      <p>Set your e-mail address.</p>
+      <h3 class="subtitle">E-mail address</h3>
+      <p>Enter your e-mail address.</p>
       <p><input name="mail" value="#{CGI::escapeHTML(@conf.mail)}" size="40"></p>
-      <h3 class="subtitle">Send mail for changes.</h3>
-      <p>If ON, notice-mail is sended to "e-mail address" of Basic preferences via SMTP server(which set in config.rb) when a page is updated. If OFF, e-mail isn't sended.</p>
+      <h3 class="subtitle">Send update e-mails?</h3>
+      <p>Set whether or not you want to have e-mail sent when a page is updated.  E-mail will be sent to the address set in the Basic Preferences.  (Make sure to specify an SMTP server beforehand in config.rb.)</p>
       <p><select name="mail_on_update">
-         <option value="true"#{@conf.mail_on_update ? ' selected' : ''}>ON</option>
-         <option value="false"#{@conf.mail_on_update ? '' : ' selected'}>OFF</option>
+         <option value="true"#{@conf.mail_on_update ? ' selected' : ''}>Yes</option>
+         <option value="false"#{@conf.mail_on_update ? '' : ' selected'}>No</option>
          </select></p>
   HTML
 end
@@ -29,10 +29,10 @@ add_conf_proc( 'password', 'Password' ) do
   saveconf_password
   <<-HTML
       <h3 class="password">Password</h3>
-      <p>Input passwords below when you want to change the password only.</p>
+      <p>Only enter passwords when you want to change the admin password.</p>
       <p>Current password: <input type="password" name="old_password" size="40"></p>
       <p>New password: <input type="password" name="password1" size="40"></p>
-      <p>New password(confirm): <input type="password" name="password2" size="40"></p>
+      <p>New password (confirm): <input type="password" name="password2" size="40"></p>
   HTML
 end
 
@@ -40,7 +40,7 @@ add_conf_proc( 'theme', 'Appearance' ) do
   saveconf_theme
   r = <<-HTML
       <h3 class="subtitle">Theme</h3>
-      <p>Select a theme.
+      <p>Select a theme to use in displaying pages.
       <p><select name="theme">
   HTML
   @conf_theme_list.each do |theme|
@@ -49,28 +49,28 @@ add_conf_proc( 'theme', 'Appearance' ) do
   r << <<-HTML
       </select></p>
       <h3 class="subtitle">Theme URL</h3>
-      <p>Enter the URL of a theme.</p>
+      <p>Specify a URL where a theme is located.  If you specify a CSS URL, the theme selected above will be ignored, and the CSS will be used.</p>
       <p><input name="theme_url" value="#{CGI::escapeHTML(@conf.theme_url)}" size="60"></p>
       <h3 class="subtitle">Theme directory</h3>
-      <p>Enter the directory of themes.</p>
+      <p>Enter the directory where themes are located.</p>
       <p><input name="theme_path" value="#{CGI::escapeHTML(@conf.theme_path)}" size="60"></p>
       <h3 class="subtitle">Sidebar</h3>
-      <p>ON if Sidebar is shown. If you want to use a theme which doesn't support Sidebar, you need to select OFF here.</p>
+      <p>Some themes cannot properly display the sidebar.  If you are using one of these themes, set this value to off.</p> 
       <p><select name="sidebar">
-         <option value="true"#{@conf.use_sidebar ? ' selected' : ''}>ON</option>
-         <option value="false"#{@conf.use_sidebar ? '' : ' selected'}>OFF</option>
+         <option value="true"#{@conf.use_sidebar ? ' selected' : ''}>On</option>
+         <option value="false"#{@conf.use_sidebar ? '' : ' selected'}>Off</option>
          </select></p>
-      <h3 class="subtitle">Class name of the main area (CSS)</h3>
-      <p>Enter the class name of the main area (default: 'main').</p>
+      <h3 class="subtitle">CSS class name for the main area</h3>
+      <p>Enter the CSS class name for the main area (default: 'main').</p>
       <p><input name="main_class" value="#{CGI::escapeHTML(@conf.main_class)}" size="20"></p>
-      <h3 class="subtitle">Class name of the side bar (CSS)</h3>
-      <p>Enter the class name of the side bar (default: 'sidebar').</p>
+      <h3 class="subtitle">CSS class name for the sidebar</h3>
+      <p>Enter the CSS class name for the sidebar (default: 'sidebar').</p>
       <p><input name="sidebar_class" value="#{CGI::escapeHTML(@conf.sidebar_class)}" size="20"></p>
       <h3 class="subtitle">Auto link</h3>
-      <p>If you want to use the auto link function, select 'On' here.</p>
+      <p>If you want to use the auto link function, set this value to on.</p>
       <p><select name="auto_link">
-         <option value="true"#{@conf.auto_link ? ' selected' : ''}>ON</option>
-         <option value="false"#{@conf.auto_link ? '' : ' selected'}>OFF</option>
+         <option value="true"#{@conf.auto_link ? ' selected' : ''}>On</option>
+         <option value="false"#{@conf.auto_link ? '' : ' selected'}>Off</option>
          </select></p>
   HTML
 end
