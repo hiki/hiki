049553e36c1a4e8e24dfc964384ba3c2789cfe3d
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-06-27 13:42:37 +0000

    * misc/plugin/isbn.rb (isbn): revise bk1's uri. remove 'Fujisan'.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@629 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- misc/plugin/isbn.rb
+++ misc/plugin/isbn.rb
@@ -1,4 +1,4 @@
-# $Id: isbn.rb,v 1.2 2004-02-15 02:48:35 hitoshi Exp $
+# $Id: isbn.rb,v 1.3 2005-06-27 13:42:37 fdiary Exp $
 # converts from sns isbn.pl
 # Copyright (C) 2003 NAKAMURA Noritsugu <nnakamur@mxq.mesh.ne.jp>
 # add Amazon Associate ID by Kazuhiko <kazuhiko@fdiary.net>
@@ -19,12 +19,11 @@ def isbn(isbn, bookname = "")
   s << buf
   s << "¡Ú "
   s << make_anchor( "http://www.amazon.co.jp/exec/obidos/ASIN/#{isbn2}#{aid}/ref=nosim/", 'amazon' ) + ' / '
-  s << make_anchor( "http://www.bk1.co.jp/cgi-bin/srch/srch_result_book.cgi?idx=3&amp;isbn=#{isbn1}", 'bk1' ) + ' / '
+  s << make_anchor( "http://www.bk1.co.jp/search/search.asp?srch=2&amp;kywd=&amp;ti=&amp;au=&amp;pb=&amp;isbn=#{isbn1}&amp;idx=1", 'bk1' ) + ' / '
   s << make_anchor( "http://www.netdirect.co.jp/search/ISSSchDetail.asp?ISBN=#{isbn2}", '°°²°' ) + ' / '
   s << make_anchor( "http://www.jbook.co.jp/product.asp?isbn=#{isbn2}", 'Jbook' ) + ' / '
   s << make_anchor( "http://bookweb.kinokuniya.co.jp/guest/cgi-bin/wshosea.cgi?W-ISBN=#{isbn2}", 'µª°ËÔ¢²°' ) + ' / '
   s << make_anchor( "http://www.esbooks.co.jp/bks.svl?CID=BKS504&amp;access_method=isbn_cd&amp;input_data=#{isbn1}", 'eS!' ) + ' / '
-  s << make_anchor( "http://bsearch.rakuten.co.jp/Btitles?KEY=#{isbn1}", '³ÚÅ·' ) + ' / '
-  s << make_anchor( "http://www.fujisan.com/online/Jsearch?key=productcode&amp;value=#{isbn2}", 'ÉÙ»Î»³' )
+  s << make_anchor( "http://bsearch.rakuten.co.jp/Btitles?KEY=#{isbn1}", '³ÚÅ·' )
   s << " ¡Û"
 end
