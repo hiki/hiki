93d9182287a2ad656263031ced37234789889a2b
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-06-27 05:54:26 +0000

    * misc/plugin/about.rb, remap_headings.rb: removed.         * misc/plugin/quote_page.rb: merge remap_headings.rb.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@625 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- misc/plugin/frozenmark.rb
+++ misc/plugin/frozenmark.rb
@@ -1,12 +1,5 @@
-# $Id: frozenmark.rb,v 1.4 2005-03-03 15:53:55 fdiary Exp $
+# $Id: frozenmark.rb,v 1.5 2005-06-27 05:54:26 fdiary Exp $
 # Copyright (C) 2003 OZAWA Sakuro <crouton@users.sourceforge.jp>
-
-def about_frozenmark
-  <<-EOS
-!Description
-When this plugin is installed, an message telling the page is frozen is displayed on all frozen pages.
-  EOS
-end
 
 add_page_attribute_proc {
   if @db.is_frozen?(@page) then
--- misc/plugin/quote_page.rb
+++ misc/plugin/quote_page.rb
@@ -1,23 +1,10 @@
-# $Id: quote_page.rb,v 1.3 2004-12-24 16:53:33 koma2 Exp $
+# $Id: quote_page.rb,v 1.4 2005-06-27 05:54:26 fdiary Exp $
 # Copyright (C) 2003 OZAWA Sakuro <crouton@users.sourceforge.jp>
 
 add_body_enter_proc {
   @quote_page_quoted = []
   ''
 }
-
-def about_quote_page
-  <<-EOS
-!Syntax
- {{quote_page(PAGE[, TOP_WANTED=1])}}
-
-!Description
-This plugin quotes other page in current page.
-
-Optional TOP_WANTED argument determines which level the headings
-in original text are mapped to in formatted result.
-  EOS
-end
 
 def quote_page(name, top_wanted=1)
   unless @quote_page_quoted.include?(name)
@@ -29,3 +16,22 @@ def quote_page(name, top_wanted=1)
   end
 end
 
+def remap_headings(tokens, top_mapped=1)
+  top_real = tokens.select {|t| /heading([1-5])_(open|close)/ =~ t[:e].id2name
+}.collect{|t| t[:lv]}.min 
+  if top_real
+    tokens.collect do |t|
+      if /heading([1-5])_(open|close)/ =~ t[:e].id2name
+        lv = t[:lv] + (top_mapped - top_real)
+        lv = [[lv, 1].max, 5].min
+        {:e => "heading#{lv}_#{$2}".intern, :lv => lv}
+      else
+        t
+      end
+    end
+  else
+    tokens
+  end
+end
+
+export_plugin_methods(:quote_page)
