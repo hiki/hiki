5f5d38a1b5915aee2bbb3f45f3caa1031708f6cc
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-06-29 06:20:09 +0000

    revise HIKI_RELEASE_DATE
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@645 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- hiki/config.rb
+++ hiki/config.rb
@@ -1,10 +1,10 @@
-# $Id: config.rb,v 1.64 2005-06-27 07:43:18 fdiary Exp $
+# $Id: config.rb,v 1.65 2005-06-29 06:20:09 fdiary Exp $
 # Copyright (C) 2004-2005 Kazuhiko <kazuhiko@fdiary.net>
 #
 # TADA Tadashi <sho@spc.gr.jp> holds the copyright of Config class.
 
 HIKI_VERSION  = '0.8.0'
-HIKI_RELEASE_DATE = '2005-06-23'
+HIKI_RELEASE_DATE = '2005-06-29'
 
 require 'cgi'
 require 'hiki/command'
