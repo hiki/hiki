f9add00d08274cac60d108d10f2d67ac40d0b622
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-06-29 06:16:20 +0000

    * plugin/02help.rb, plugin/en/02help.rb, plugin/ja/02help.rb: add         a new plugin for support editing (by NAGASAKI).
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@644 684b846f-27f2-41ea-b66a-77a1bafa84c1

---
+++ plugin/02help.rb
@@ -0,0 +1,320 @@
+#===== hiki_help
+if (@conf.style == "default" || @conf.style == "math") && !@conf.mobile_agent?
+   @options['help.ptlbr'] = true
+   @options['help.tlbr_class'] ||= "helptlbr"
+   @options['help.bttn_class'] ||= "helpbttn"
+   add_edit_proc {
+      hiki_help
+   }
+end
+
+def hiki_help
+   help = <<END_HELP
+
+<script type="text/javascript"><!--
+
+var tlbr_class = "#{@options['help.tlbr_class']}"
+var bttn_class = "#{@options['help.bttn_class']}"
+
+var heading_label = "#{help_heading_label}"
+var list_label = "#{help_list_label}"
+var numbered_label = "#{help_numbered_label}"
+var link_label = "#{help_link_label}"
+var url_label = "#{help_url_label}"
+var emphasized_label = "#{help_emphasized_label}"
+var strongly_label = "#{help_strongly_label}"
+var struckout_label = "#{help_struckout_label}"
+var definition_label = "#{help_definition_label}"
+var table_label = "#{help_table_label}"
+var cell_label = "#{help_cell_label}"
+var headingcell_label = "#{help_headingcell_label}"
+var rows_label = "#{help_rows_label}"
+var columns_label = "#{help_columns_label}"
+var horizontal_label = "#{help_horizontal_label}"
+var multiplelines_label = "#{help_multiplelines_label}"
+var preformatted_label = "#{help_preformatted_label}"
+var cancel_label = "#{help_cancel_label}"
+var quotation_label = "#{help_quotation_label}"
+var comment_label = "#{help_comment_label}"
+var math_label = "#{help_math_label}"
+var display_label = "#{help_display_label}"
+var inline_label = "#{help_inline_label}"
+var plugin_label = "#{help_plugin_label}"
+var br_label = "#{help_br_label}"
+var toc_label = "#{help_toc_label}"
+var tochere_label = "#{help_tochere_label}"
+var recent_label = "#{help_recent_label}"
+
+function set_s( pre, suf, mg ){
+   txtarea.focus();
+   if ( typeof(document["selection"]) != "undefined" ) {
+   // for IE, Opera ?
+      var rng = document.selection.createRange();
+      var str = rng.text
+      switch( mg ) {
+      case 0:
+         rng.text = pre + str + suf;
+         break;
+      case 1:
+         str = str.replace(/\\r/mg, "");
+         rng.text = "\\n" + str.replace(/^/mg, pre) + "\\n";
+         break;
+      case 2:
+         var re = new RegExp("^" + pre, "mg");
+         rng.text = str.replace( re, "");
+         break;
+      }
+   } else if ( typeof(txtarea["setSelectionRange"]) != "undefined" ) {
+   // for Mozilla ?
+      var start = txtarea.selectionStart;
+      var end = txtarea.selectionEnd;
+      var scrollPos = txtarea.scrollTop;
+      var str = txtarea.value.substring( start, end );
+      var j;
+      switch( mg ) {
+      case 0:
+         j = pre + str + suf;
+         break;
+      case 1:
+         j = "\\n" + str.replace( /^/mg, pre) + "\\n";
+         break;
+      case 2:
+         var re = new RegExp("^" + pre, "mg");
+         j = str.replace( re, "");
+         break;
+      }
+      txtarea.value = txtarea.value.substring(0, start) + j + txtarea.value.substring(end);
+      if ( str ) {
+         txtarea.setSelectionRange( start + j.length, start + j.length);
+      } else {
+         txtarea.setSelectionRange(start + pre.length, start + pre.length);
+      }
+      txtarea.scrollTop = scrollPos;
+   }
+   txtarea.focus();
+}
+
+function set_bttn( tlbr, txt, tps, help ) {
+   var sp = document.createElement("span");
+   sp.className = bttn_class;
+   var a = document.createElement("a");
+   a.href = help;
+   a.title = tps;
+   var atxt = document.createTextNode(txt);
+   a.appendChild(atxt);
+   sp.appendChild(a);
+   tlbr.appendChild(sp);
+}
+
+function add_bttn( tlbr, txt, tps, pre, suf ) {
+   var help = "javascript:set_s( '" + pre + "', '" + suf + "', 0 )";
+   set_bttn( tlbr, txt, tps, help );
+}
+
+function add_bttn_mg( tlbr, txt, tps, pre, mg ) {
+   var help = "javascript:set_s( '" + pre + "', '', " + mg + " )";
+   set_bttn( tlbr, txt, tps, help );
+}
+
+function add_pbttn( txt, tps, pre, suf ) {
+   add_bttn( ptlbr, txt, tps, pre, suf )
+}
+
+function add_txt( tlbr, txt ) {
+   var tn = document.createTextNode( txt );
+   tlbr.appendChild( tn );
+}
+
+function add_ptxt( txt ) {
+   add_txt( ptlbr, txt )
+}
+END_HELP
+
+   help << <<'END_HELP'
+
+function set_tlbr() {
+
+   add_txt( tlbr, "[" + heading_label + "|" );
+   add_bttn( tlbr, "1", "!TEXT", "\\n!", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, "2", "!!TEXT", "\\n!!", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, "3", "!!!TEXT", "\\n!!!", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, "4", "!!!!TEXT", "\\n!!!!", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, "5", "!!!!!TEXT", "\\n!!!!!", "" );
+   add_txt( tlbr, "]" );
+
+   add_txt( tlbr, "[" + list_label + "|" );
+   add_bttn( tlbr, "1", "*TEXT", "*", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, "2", "**TEXT", "**", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, "3", "***TEXT", "***", "" );
+   add_txt( tlbr, "|" + numbered_label + "|" );
+   add_bttn( tlbr, "1", "#TEXT", "#", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, "2", "##TEXT", "##", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, "3", "###TEXT", "###", "" );
+   add_txt( tlbr, "]" );
+
+   add_txt( tlbr, "[" );
+   add_bttn( tlbr, link_label, "[[TEXT]]", "[[", "]]" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, url_label, "[[TEXT|http://]]", "[[", "|http://]]" );
+   add_txt( tlbr, "]" );
+
+   add_txt( tlbr, "[" );
+   add_bttn( tlbr, emphasized_label, "\'\'TEXT\'\'", "\\'\\'", "\\'\\'" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, strongly_label, "\'\'\'TEXT\'\'\'", "\\'\\'\\'", "\\'\\'\\'" );
+   add_txt( tlbr, "]" );
+   
+   add_txt( tlbr, "[" );
+   add_bttn( tlbr, struckout_label, "==TEXT==", "==", "==" );
+   add_txt( tlbr, "]" );
+
+   add_txt( tlbr, "[" );
+   add_bttn( tlbr, definition_label, ":WORD:TEXT", "\\n:", ":" );
+   add_txt( tlbr, "]" );
+
+   add_txt( tlbr, "[" + table_label + "|" );
+   add_bttn( tlbr, cell_label, "||TEXT", "||", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, headingcell_label, "||!TEXT", "!", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, rows_label, "||^TEXT", "^", "" );
+   add_txt( tlbr, "|" );
+   add_bttn( tlbr, columns_label, "||>TEXT", ">", "" );
+   add_txt( tlbr, "]" );
+
+   add_txt( tlbr, "[" );
+   add_bttn( tlbr, horizontal_label, "----", "\\n----\\n", "" );
+   add_txt( tlbr, "]" );
+
+}
+
+function set_mtlbr() {
+
+   add_txt( mtlbr, "<" + multiplelines_label + ">" );
+
+   add_txt( mtlbr, "[" );
+   add_bttn_mg( mtlbr, preformatted_label, " TEXT\\n TEXT", " ", 1 );
+   add_txt( mtlbr, "\(" );
+   add_bttn_mg( mtlbr, cancel_label, "TEXT\nTEXT", " ", 2 );
+   add_txt( mtlbr, "\)" );
+   add_txt( mtlbr, "]" );
+
+   add_txt( mtlbr, "[" );
+   add_bttn_mg( mtlbr, quotation_label, "\"\"TEXT\n\"\"TEXT", "\\\"\\\"", 1 );
+   add_txt( mtlbr, "\(" );
+   add_bttn_mg( mtlbr, cancel_label, "TEXT\nTEXT", "\\\"\\\"", 2 );
+   add_txt( mtlbr, "\)" );
+   add_txt( mtlbr, "]" );
+
+   add_txt( mtlbr, "[" );
+   add_bttn_mg( mtlbr, comment_label, "//TEXT\n//TEXT", "//", 1 );
+   add_txt( mtlbr, "\(" );
+   add_bttn_mg( mtlbr, cancel_label, "TEXT\nTEXT", "//", 2 );
+   add_txt( mtlbr, "\)" );
+   add_txt( mtlbr, "]" );
+
+}
+
+function set_mathtlbr() {
+   add_txt( mtlbr, "[" + math_label + "|" );
+   add_bttn_mg( mtlbr, display_label, "$$TEXT\n$$TEXT", "$$$", 1 );
+   add_txt( mtlbr, "\(" );
+   add_bttn_mg( mtlbr, cancel_label, "TEXT\nTEXT", "\\\\$\\\\$", 2 );
+   add_txt( mtlbr, "\)" );
+   add_txt( mtlbr, "|" );
+   add_bttn( mtlbr, inline_label, "[$TEXT$]", "[$", "$]" );
+   add_txt( mtlbr, "]" );
+}
+
+function set_ptlbr() {
+
+   add_ptxt( "[" );
+   add_pbttn( plugin_label, "{{TEXT}}", "{{", "}}" );
+   add_ptxt( "]" );
+
+   add_ptxt( "[" );
+   add_pbttn( br_label, "{{br}}", "{{br}}", "" );
+   add_ptxt( "|" );
+   add_pbttn( toc_label, "{{toc}}", "\\n{{toc}}\\n", "" );
+   add_ptxt( "|" );
+   add_pbttn( tochere_label, "{{toc_here}}", "\\n{{toc_here}}\\n", "" );
+   add_ptxt( "|" );
+   add_pbttn( recent_label, "{{recent\(num\)}}", "\\n{{recent\(20\)}}\\n", "" );
+   add_ptxt( "]" );
+
+}
+
+function view_tlbr( tlbr ) {
+   tlbr.className = tlbr_class;
+   txtarea.parentNode.insertBefore(tlbr, txtarea);
+}
+END_HELP
+
+   help << <<END_HELP
+
+// main
+   var txtarea = document.forms[0].contents;
+   var tlbr = document.createElement("div");
+   var mtlbr = document.createElement("div");
+   var ptlbr = document.createElement("div");
+
+   set_tlbr()
+   view_tlbr( tlbr )
+
+   set_mtlbr()
+END_HELP
+
+if @conf.style == "math"
+   help << <<END_HELP
+   set_mathtlbr()
+END_HELP
+end
+
+   help << <<END_HELP
+
+   view_tlbr( mtlbr )
+
+   set_ptlbr()
+   view_tlbr( ptlbr )
+
+// --></script>
+END_HELP
+   help
+end
+
+#----- for other plugin
+def help_add_pbttn( help_txt, help_tps, help_pre, help_suf )
+
+   if @options['help.ptlbr']
+      add_edit_proc do
+         help = <<END_HELP
+
+<script type="text/javascript"><!--
+
+   var txt = "#{help_txt}"
+   var tps = "#{help_tps}"
+   var pre = "#{help_pre}"
+   var suf = "#{help_suf}"
+
+   add_ptxt( "[" );
+   add_pbttn( txt, tps, pre, suf );
+   add_ptxt( "]" );
+
+// --></script>
+
+END_HELP
+         help
+      end
+   end
+end
+
+# export no methods
+export_plugin_methods
---
+++ plugin/ja/02help.rb
@@ -0,0 +1,111 @@
+def help_heading_label
+   '見出し'
+end
+
+def help_list_label
+   '箇条書'
+end
+
+def help_numbered_label
+   '番号付'
+end
+
+def help_link_label
+   'リンク'
+end
+
+def help_url_label
+   'URL'
+end
+
+def help_emphasized_label
+   '強調'
+end
+
+def help_strongly_label
+   'さらに'
+end
+
+def help_struckout_label
+   '取消線'
+end
+
+def help_definition_label
+   '用語解説'
+end
+
+def help_table_label
+   '表'
+end
+
+def help_cell_label
+   'セル'
+end
+
+def help_headingcell_label
+   '見出し'
+end
+
+def help_rows_label
+   '縦連結'
+end
+
+def help_columns_label
+   '横連結'
+end
+
+def help_horizontal_label
+   '水平線'
+end
+
+def help_multiplelines_label
+   '複数行指定'
+end
+
+def help_preformatted_label
+   '整形済'
+end
+
+def help_cancel_label
+   '解除'
+end
+
+def help_quotation_label
+   '引用'
+end
+
+def help_comment_label
+   'コメント'
+end
+
+def help_math_label
+   '数式'
+end
+
+def help_display_label
+   'ディスプレイ'
+end
+
+def help_inline_label
+   'インライン'
+end
+
+def help_plugin_label
+   'プラグイン'
+end
+
+def help_br_label
+   '改行'
+end
+
+def help_toc_label
+   '目次'
+end
+
+def help_tochere_label
+   'ここに'
+end
+
+def help_recent_label
+   '最近'
+end
