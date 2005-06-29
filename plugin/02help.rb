2a28c4fa1da5929888ac09e65a27b8296d4000b6
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-06-29 07:24:19 +0000

    * plugin/02help.rb (help_enabled?): add a new method.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@648 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- plugin/02help.rb
+++ plugin/02help.rb
@@ -1,15 +1,11 @@
-#===== hiki_help
-if (@conf.style == "default" || @conf.style == "math") && !@conf.mobile_agent?
-   @options['help.ptlbr'] = true
-   @options['help.tlbr_class'] ||= "helptlbr"
-   @options['help.bttn_class'] ||= "helpbttn"
-   add_edit_proc {
-      hiki_help
-   }
+def help_enabled?
+  (@conf.style == "default" || @conf.style == "math") && !@conf.mobile_agent?
 end
 
 def hiki_help
-   help = <<END_HELP
+  @options['help.tlbr_class'] ||= "helptlbr"
+  @options['help.bttn_class'] ||= "helpbttn"
+  help = <<END_HELP
 
 <script type="text/javascript"><!--
 
@@ -46,274 +42,277 @@ var tochere_label = "#{help_tochere_label}"
 var recent_label = "#{help_recent_label}"
 
 function set_s( pre, suf, mg ){
-   txtarea.focus();
-   if ( typeof(document["selection"]) != "undefined" ) {
-   // for IE, Opera ?
-      var rng = document.selection.createRange();
-      var str = rng.text
-      switch( mg ) {
-      case 0:
-         rng.text = pre + str + suf;
-         break;
-      case 1:
-         str = str.replace(/\\r/mg, "");
-         rng.text = "\\n" + str.replace(/^/mg, pre) + "\\n";
-         break;
-      case 2:
-         var re = new RegExp("^" + pre, "mg");
-         rng.text = str.replace( re, "");
-         break;
-      }
-   } else if ( typeof(txtarea["setSelectionRange"]) != "undefined" ) {
-   // for Mozilla ?
-      var start = txtarea.selectionStart;
-      var end = txtarea.selectionEnd;
-      var scrollPos = txtarea.scrollTop;
-      var str = txtarea.value.substring( start, end );
-      var j;
-      switch( mg ) {
-      case 0:
-         j = pre + str + suf;
-         break;
-      case 1:
-         j = "\\n" + str.replace( /^/mg, pre) + "\\n";
-         break;
-      case 2:
-         var re = new RegExp("^" + pre, "mg");
-         j = str.replace( re, "");
-         break;
-      }
-      txtarea.value = txtarea.value.substring(0, start) + j + txtarea.value.substring(end);
-      if ( str ) {
-         txtarea.setSelectionRange( start + j.length, start + j.length);
-      } else {
-         txtarea.setSelectionRange(start + pre.length, start + pre.length);
-      }
-      txtarea.scrollTop = scrollPos;
-   }
-   txtarea.focus();
+  txtarea.focus();
+  if ( typeof(document["selection"]) != "undefined" ) {
+  // for IE, Opera ?
+    var rng = document.selection.createRange();
+    var str = rng.text
+    switch( mg ) {
+    case 0:
+      rng.text = pre + str + suf;
+      break;
+    case 1:
+      str = str.replace(/\\r/mg, "");
+      rng.text = "\\n" + str.replace(/^/mg, pre) + "\\n";
+      break;
+    case 2:
+      var re = new RegExp("^" + pre, "mg");
+      rng.text = str.replace( re, "");
+      break;
+    }
+  } else if ( typeof(txtarea["setSelectionRange"]) != "undefined" ) {
+  // for Mozilla ?
+    var start = txtarea.selectionStart;
+    var end = txtarea.selectionEnd;
+    var scrollPos = txtarea.scrollTop;
+    var str = txtarea.value.substring( start, end );
+    var j;
+    switch( mg ) {
+    case 0:
+      j = pre + str + suf;
+      break;
+    case 1:
+      j = "\\n" + str.replace( /^/mg, pre) + "\\n";
+      break;
+    case 2:
+      var re = new RegExp("^" + pre, "mg");
+      j = str.replace( re, "");
+      break;
+    }
+    txtarea.value = txtarea.value.substring(0, start) + j + txtarea.value.substring(end);
+    if ( str ) {
+      txtarea.setSelectionRange( start + j.length, start + j.length);
+    } else {
+      txtarea.setSelectionRange(start + pre.length, start + pre.length);
+    }
+    txtarea.scrollTop = scrollPos;
+  }
+  txtarea.focus();
 }
 
 function set_bttn( tlbr, txt, tps, help ) {
-   var sp = document.createElement("span");
-   sp.className = bttn_class;
-   var a = document.createElement("a");
-   a.href = help;
-   a.title = tps;
-   var atxt = document.createTextNode(txt);
-   a.appendChild(atxt);
-   sp.appendChild(a);
-   tlbr.appendChild(sp);
+  var sp = document.createElement("span");
+  sp.className = bttn_class;
+  var a = document.createElement("a");
+  a.href = help;
+  a.title = tps;
+  var atxt = document.createTextNode(txt);
+  a.appendChild(atxt);
+  sp.appendChild(a);
+  tlbr.appendChild(sp);
 }
 
 function add_bttn( tlbr, txt, tps, pre, suf ) {
-   var help = "javascript:set_s( '" + pre + "', '" + suf + "', 0 )";
-   set_bttn( tlbr, txt, tps, help );
+  var help = "javascript:set_s( '" + pre + "', '" + suf + "', 0 )";
+  set_bttn( tlbr, txt, tps, help );
 }
 
 function add_bttn_mg( tlbr, txt, tps, pre, mg ) {
-   var help = "javascript:set_s( '" + pre + "', '', " + mg + " )";
-   set_bttn( tlbr, txt, tps, help );
+  var help = "javascript:set_s( '" + pre + "', '', " + mg + " )";
+  set_bttn( tlbr, txt, tps, help );
 }
 
 function add_pbttn( txt, tps, pre, suf ) {
-   add_bttn( ptlbr, txt, tps, pre, suf )
+  add_bttn( ptlbr, txt, tps, pre, suf )
 }
 
 function add_txt( tlbr, txt ) {
-   var tn = document.createTextNode( txt );
-   tlbr.appendChild( tn );
+  var tn = document.createTextNode( txt );
+  tlbr.appendChild( tn );
 }
 
 function add_ptxt( txt ) {
-   add_txt( ptlbr, txt )
+  add_txt( ptlbr, txt )
 }
 END_HELP
 
-   help << <<'END_HELP'
+  help << <<'END_HELP'
 
 function set_tlbr() {
 
-   add_txt( tlbr, "[" + heading_label + "|" );
-   add_bttn( tlbr, "1", "!TEXT", "\\n!", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, "2", "!!TEXT", "\\n!!", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, "3", "!!!TEXT", "\\n!!!", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, "4", "!!!!TEXT", "\\n!!!!", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, "5", "!!!!!TEXT", "\\n!!!!!", "" );
-   add_txt( tlbr, "]" );
+  add_txt( tlbr, "[" + heading_label + "|" );
+  add_bttn( tlbr, "1", "!TEXT", "\\n!", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, "2", "!!TEXT", "\\n!!", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, "3", "!!!TEXT", "\\n!!!", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, "4", "!!!!TEXT", "\\n!!!!", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, "5", "!!!!!TEXT", "\\n!!!!!", "" );
+  add_txt( tlbr, "]" );
 
-   add_txt( tlbr, "[" + list_label + "|" );
-   add_bttn( tlbr, "1", "*TEXT", "*", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, "2", "**TEXT", "**", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, "3", "***TEXT", "***", "" );
-   add_txt( tlbr, "|" + numbered_label + "|" );
-   add_bttn( tlbr, "1", "#TEXT", "#", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, "2", "##TEXT", "##", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, "3", "###TEXT", "###", "" );
-   add_txt( tlbr, "]" );
+  add_txt( tlbr, "[" + list_label + "|" );
+  add_bttn( tlbr, "1", "*TEXT", "*", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, "2", "**TEXT", "**", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, "3", "***TEXT", "***", "" );
+  add_txt( tlbr, "|" + numbered_label + "|" );
+  add_bttn( tlbr, "1", "#TEXT", "#", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, "2", "##TEXT", "##", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, "3", "###TEXT", "###", "" );
+  add_txt( tlbr, "]" );
 
-   add_txt( tlbr, "[" );
-   add_bttn( tlbr, link_label, "[[TEXT]]", "[[", "]]" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, url_label, "[[TEXT|http://]]", "[[", "|http://]]" );
-   add_txt( tlbr, "]" );
+  add_txt( tlbr, "[" );
+  add_bttn( tlbr, link_label, "[[TEXT]]", "[[", "]]" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, url_label, "[[TEXT|http://]]", "[[", "|http://]]" );
+  add_txt( tlbr, "]" );
 
-   add_txt( tlbr, "[" );
-   add_bttn( tlbr, emphasized_label, "\'\'TEXT\'\'", "\\'\\'", "\\'\\'" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, strongly_label, "\'\'\'TEXT\'\'\'", "\\'\\'\\'", "\\'\\'\\'" );
-   add_txt( tlbr, "]" );
-   
-   add_txt( tlbr, "[" );
-   add_bttn( tlbr, struckout_label, "==TEXT==", "==", "==" );
-   add_txt( tlbr, "]" );
+  add_txt( tlbr, "[" );
+  add_bttn( tlbr, emphasized_label, "\'\'TEXT\'\'", "\\'\\'", "\\'\\'" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, strongly_label, "\'\'\'TEXT\'\'\'", "\\'\\'\\'", "\\'\\'\\'" );
+  add_txt( tlbr, "]" );
+  
+  add_txt( tlbr, "[" );
+  add_bttn( tlbr, struckout_label, "==TEXT==", "==", "==" );
+  add_txt( tlbr, "]" );
 
-   add_txt( tlbr, "[" );
-   add_bttn( tlbr, definition_label, ":WORD:TEXT", "\\n:", ":" );
-   add_txt( tlbr, "]" );
+  add_txt( tlbr, "[" );
+  add_bttn( tlbr, definition_label, ":WORD:TEXT", "\\n:", ":" );
+  add_txt( tlbr, "]" );
 
-   add_txt( tlbr, "[" + table_label + "|" );
-   add_bttn( tlbr, cell_label, "||TEXT", "||", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, headingcell_label, "||!TEXT", "!", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, rows_label, "||^TEXT", "^", "" );
-   add_txt( tlbr, "|" );
-   add_bttn( tlbr, columns_label, "||>TEXT", ">", "" );
-   add_txt( tlbr, "]" );
+  add_txt( tlbr, "[" + table_label + "|" );
+  add_bttn( tlbr, cell_label, "||TEXT", "||", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, headingcell_label, "||!TEXT", "!", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, rows_label, "||^TEXT", "^", "" );
+  add_txt( tlbr, "|" );
+  add_bttn( tlbr, columns_label, "||>TEXT", ">", "" );
+  add_txt( tlbr, "]" );
 
-   add_txt( tlbr, "[" );
-   add_bttn( tlbr, horizontal_label, "----", "\\n----\\n", "" );
-   add_txt( tlbr, "]" );
+  add_txt( tlbr, "[" );
+  add_bttn( tlbr, horizontal_label, "----", "\\n----\\n", "" );
+  add_txt( tlbr, "]" );
 
 }
 
 function set_mtlbr() {
 
-   add_txt( mtlbr, "<" + multiplelines_label + ">" );
+  add_txt( mtlbr, "<" + multiplelines_label + ">" );
 
-   add_txt( mtlbr, "[" );
-   add_bttn_mg( mtlbr, preformatted_label, " TEXT\\n TEXT", " ", 1 );
-   add_txt( mtlbr, "\(" );
-   add_bttn_mg( mtlbr, cancel_label, "TEXT\nTEXT", " ", 2 );
-   add_txt( mtlbr, "\)" );
-   add_txt( mtlbr, "]" );
+  add_txt( mtlbr, "[" );
+  add_bttn_mg( mtlbr, preformatted_label, " TEXT\\n TEXT", " ", 1 );
+  add_txt( mtlbr, "\(" );
+  add_bttn_mg( mtlbr, cancel_label, "TEXT\\nTEXT", " ", 2 );
+  add_txt( mtlbr, "\)" );
+  add_txt( mtlbr, "]" );
 
-   add_txt( mtlbr, "[" );
-   add_bttn_mg( mtlbr, quotation_label, "\"\"TEXT\n\"\"TEXT", "\\\"\\\"", 1 );
-   add_txt( mtlbr, "\(" );
-   add_bttn_mg( mtlbr, cancel_label, "TEXT\nTEXT", "\\\"\\\"", 2 );
-   add_txt( mtlbr, "\)" );
-   add_txt( mtlbr, "]" );
+  add_txt( mtlbr, "[" );
+  add_bttn_mg( mtlbr, quotation_label, "\"\"TEXT\\n\"\"TEXT", "\\\"\\\"", 1 );
+  add_txt( mtlbr, "\(" );
+  add_bttn_mg( mtlbr, cancel_label, "TEXT\\nTEXT", "\\\"\\\"", 2 );
+  add_txt( mtlbr, "\)" );
+  add_txt( mtlbr, "]" );
 
-   add_txt( mtlbr, "[" );
-   add_bttn_mg( mtlbr, comment_label, "//TEXT\n//TEXT", "//", 1 );
-   add_txt( mtlbr, "\(" );
-   add_bttn_mg( mtlbr, cancel_label, "TEXT\nTEXT", "//", 2 );
-   add_txt( mtlbr, "\)" );
-   add_txt( mtlbr, "]" );
+  add_txt( mtlbr, "[" );
+  add_bttn_mg( mtlbr, comment_label, "//TEXT\\n//TEXT", "//", 1 );
+  add_txt( mtlbr, "\(" );
+  add_bttn_mg( mtlbr, cancel_label, "TEXT\\nTEXT", "//", 2 );
+  add_txt( mtlbr, "\)" );
+  add_txt( mtlbr, "]" );
 
 }
 
 function set_mathtlbr() {
-   add_txt( mtlbr, "[" + math_label + "|" );
-   add_bttn_mg( mtlbr, display_label, "$$TEXT\n$$TEXT", "$$$", 1 );
-   add_txt( mtlbr, "\(" );
-   add_bttn_mg( mtlbr, cancel_label, "TEXT\nTEXT", "\\\\$\\\\$", 2 );
-   add_txt( mtlbr, "\)" );
-   add_txt( mtlbr, "|" );
-   add_bttn( mtlbr, inline_label, "[$TEXT$]", "[$", "$]" );
-   add_txt( mtlbr, "]" );
+  add_txt( mtlbr, "[" + math_label + "|" );
+  add_bttn_mg( mtlbr, display_label, "$$TEXT\\n$$TEXT", "$$$", 1 );
+  add_txt( mtlbr, "\(" );
+  add_bttn_mg( mtlbr, cancel_label, "TEXT\\nTEXT", "\\\\$\\\\$", 2 );
+  add_txt( mtlbr, "\)" );
+  add_txt( mtlbr, "|" );
+  add_bttn( mtlbr, inline_label, "[$TEXT$]", "[$", "$]" );
+  add_txt( mtlbr, "]" );
 }
 
 function set_ptlbr() {
 
-   add_ptxt( "[" );
-   add_pbttn( plugin_label, "{{TEXT}}", "{{", "}}" );
-   add_ptxt( "]" );
+  add_ptxt( "[" );
+  add_pbttn( plugin_label, "{{TEXT}}", "{{", "}}" );
+  add_ptxt( "]" );
 
-   add_ptxt( "[" );
-   add_pbttn( br_label, "{{br}}", "{{br}}", "" );
-   add_ptxt( "|" );
-   add_pbttn( toc_label, "{{toc}}", "\\n{{toc}}\\n", "" );
-   add_ptxt( "|" );
-   add_pbttn( tochere_label, "{{toc_here}}", "\\n{{toc_here}}\\n", "" );
-   add_ptxt( "|" );
-   add_pbttn( recent_label, "{{recent\(num\)}}", "\\n{{recent\(20\)}}\\n", "" );
-   add_ptxt( "]" );
+  add_ptxt( "[" );
+  add_pbttn( br_label, "{{br}}", "{{br}}", "" );
+  add_ptxt( "|" );
+  add_pbttn( toc_label, "{{toc}}", "\\n{{toc}}\\n", "" );
+  add_ptxt( "|" );
+  add_pbttn( tochere_label, "{{toc_here}}", "\\n{{toc_here}}\\n", "" );
+  add_ptxt( "|" );
+  add_pbttn( recent_label, "{{recent\(num\)}}", "\\n{{recent\(20\)}}\\n", "" );
+  add_ptxt( "]" );
 
 }
 
 function view_tlbr( tlbr ) {
-   tlbr.className = tlbr_class;
-   txtarea.parentNode.insertBefore(tlbr, txtarea);
+  tlbr.className = tlbr_class;
+  txtarea.parentNode.insertBefore(tlbr, txtarea);
 }
 END_HELP
 
-   help << <<END_HELP
+  help << <<END_HELP
 
 // main
-   var txtarea = document.forms[0].contents;
-   var tlbr = document.createElement("div");
-   var mtlbr = document.createElement("div");
-   var ptlbr = document.createElement("div");
+  var txtarea = document.forms[0].contents;
+  var tlbr = document.createElement("div");
+  var mtlbr = document.createElement("div");
+  var ptlbr = document.createElement("div");
 
-   set_tlbr()
-   view_tlbr( tlbr )
+  set_tlbr()
+  view_tlbr( tlbr )
 
-   set_mtlbr()
+  set_mtlbr()
 END_HELP
 
 if @conf.style == "math"
-   help << <<END_HELP
-   set_mathtlbr()
+  help << <<END_HELP
+  set_mathtlbr()
 END_HELP
 end
 
-   help << <<END_HELP
+  help << <<END_HELP
 
-   view_tlbr( mtlbr )
+  view_tlbr( mtlbr )
 
-   set_ptlbr()
-   view_tlbr( ptlbr )
+  set_ptlbr()
+  view_tlbr( ptlbr )
 
 // --></script>
 END_HELP
-   help
+  help
 end
 
-#----- for other plugin
 def help_add_pbttn( help_txt, help_tps, help_pre, help_suf )
-
-   if @options['help.ptlbr']
-      add_edit_proc do
-         help = <<END_HELP
+  return unless help_enabled?
+  add_edit_proc do
+    help = <<END_HELP
 
 <script type="text/javascript"><!--
 
-   var txt = "#{help_txt}"
-   var tps = "#{help_tps}"
-   var pre = "#{help_pre}"
-   var suf = "#{help_suf}"
+  var txt = "#{help_txt}"
+  var tps = "#{help_tps}"
+  var pre = "#{help_pre}"
+  var suf = "#{help_suf}"
 
-   add_ptxt( "[" );
-   add_pbttn( txt, tps, pre, suf );
-   add_ptxt( "]" );
+  add_ptxt( "[" );
+  add_pbttn( txt, tps, pre, suf );
+  add_ptxt( "]" );
 
 // --></script>
 
 END_HELP
-         help
-      end
-   end
+    help
+  end
+end
+
+if help_enabled?
+  add_edit_proc do
+    hiki_help
+  end
 end
 
 # export no methods
