4be5f9c1cf6ba03cee44e5bba5abb8fe35d89038
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-07-15 13:51:47 +0000

    * misc/plugin/en/math.rb: add English resources (by Kashia Buch).
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@697 684b846f-27f2-41ea-b66a-77a1bafa84c1

---
+++ misc/plugin/en/math.rb
@@ -0,0 +1,10 @@
+def label_math_latex_ptsize; 'Font Size (pt)'; end
+def label_math_latex_documentclass; 'Document Class'; end
+def label_math_latex_preamble; 'Preamble'; end
+def label_math_latex_log; 'Log'; end
+def label_math_latex_log_description; 'When checked, the log to standard error output during execution won\'t be deleted afterwards'; end
+def label_math_latex_cache_clear; 'Clear Cache'; end
+def label_math_latex_cache_clear_description; 'When checked, the images stored in the cache will be removed'; end
+def label_math_latex_latex; 'latex Command'; end
+def label_math_latex_dvips; 'dvips Command'; end
+def label_math_latex_convert; 'convert Command'; end
