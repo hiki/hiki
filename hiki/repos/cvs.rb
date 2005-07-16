8d834c130d91ca49032b81b28668e0a6898cd08c
yanagita <yanagita@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-07-16 01:51:52 +0000

    use Dir.chdir with a block.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@699 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- hiki/repos/cvs.rb
+++ hiki/repos/cvs.rb
@@ -1,4 +1,4 @@
-# $Id: cvs.rb,v 1.7 2005-07-14 12:32:07 yanagita Exp $
+# $Id: cvs.rb,v 1.8 2005-07-16 01:51:52 yanagita Exp $
 # Copyright (C) 2003, Koichiro Ohba <koichiro@meadowy.org>
 # Copyright (C) 2003, Yasuo Itabashi <yasuo_itabashi{@}hotmail.com>
 # You can distribute this under GPL.
@@ -8,18 +8,15 @@ require 'hiki/repos/default'
 # CVS Repository Backend
 module Hiki
   class ReposCvs < ReposBase
-    def setup()
-      oldpwd = Dir.pwd
-      begin
-        Dir.chdir( @data_path )
+    def setup
+      Dir.chdir( @data_path ) do
         system( "cvs -d #{@root} init > /dev/null 2>&1" )
-        if not File.directory?(".CVSROOT") then
+        if not File.directory?(".CVSROOT")
           system( "cvs -d #{@root} co -d .CVSROOT CVSROOT > /dev/null 2>&1" )
         end
-        Dir.chdir( ".CVSROOT" )
-        system( "cvs -d #{@root} update > /dev/null 2>&1" )
-      ensure
-        Dir.chdir( oldpwd.untaint )
+        Dir.chdir( ".CVSROOT" ) do
+          system( "cvs -d #{@root} update > /dev/null 2>&1" )
+        end
       end
     end
 
@@ -28,55 +25,40 @@ module Hiki
     end
 
     def import( wiki )
-      oldpwd = Dir.pwd
-      begin
-        Dir.chdir( "#{@data_path}/#{wiki}/text" )
+      Dir.chdir( "#{@data_path}/#{wiki}/text" ) do
         system( "cvs -d #{@root} import -m 'Starting #{wiki} from #{ENV['REMOTE_ADDR']} - #{ENV['REMOTE_HOST']}' #{wiki} hiki start > /dev/null 2>&1".untaint )
-        Dir.chdir( '..' )
-        system( "cvs -d #{@root} co -d text #{wiki} > /dev/null 2>&1" )
-      ensure
-        Dir.chdir( oldpwd.untaint )
+        Dir.chdir( '..' ) do
+          system( "cvs -d #{@root} co -d text #{wiki} > /dev/null 2>&1" )
+        end
       end
     end
 
     def update( wiki )
-      oldpwd = Dir.pwd
-      begin
-        Dir.chdir( "#{@data_path}/#{wiki}/text" )
+      Dir.chdir( "#{@data_path}/#{wiki}/text" ) do
         system( "cvs -d #{@root} update > /dev/null 2>&1" )
-      ensure
-        Dir.chdir( oldpwd.untaint )
       end
     end
 
     def commit(page, msg = default_msg)
-      oldpwd = Dir.pwd.untaint
-      begin
-        Dir.chdir( "#{@data_path}/text" )
+      Dir.chdir( "#{@data_path}/text" ) do
         system( "cvs -d #{@root} add -- #{page.escape} > /dev/null 2>&1".untaint )
         system( "cvs -d #{@root} ci -m '#{msg}' > /dev/null 2>&1".untaint )
-      ensure
-        Dir.chdir( oldpwd )
       end
     end
 
     def delete(page, msg = default_msg)
-      oldpwd = Dir.pwd.untaint
-      begin
-        Dir.chdir( "#{@data_path}/text" )
+      Dir.chdir( "#{@data_path}/text" ) do
         system( "cvs -d #{@root} remove -- #{page.escape} > /dev/null 2>&1".untaint )
         system( "cvs -d #{@root} ci -m '#{msg}' > /dev/null 2>&1".untaint )
-      ensure
-        Dir.chdir( oldpwd )
       end
     end
 
     def get_revision(page, revision)
       ret = ''
       Dir.chdir("#{@data_path}/text") do
-	open("|cvs -Q up -p -r 1.#{revision.to_i} #{page.escape.untaint}") do |f|
-	  ret = f.read
-	end
+        open("|cvs -Q up -p -r 1.#{revision.to_i} #{page.escape.untaint}") do |f|
+          ret = f.read
+        end
       end
       ret
     end
@@ -86,9 +68,9 @@ module Hiki
       log = ''
       revs = []
       Dir.chdir("#{@data_path}/text") do
-	open("|cvs -Q log #{page.escape.untaint}") do |f|
-	  log = f.read
-	end
+        open("|cvs -Q log #{page.escape.untaint}") do |f|
+          log = f.read
+        end
       end
       log.split(/----------------------------/).each do |tmp|
         if /revision 1.(\d+?)\ndate: (.*?);  author: (?:.*?);  state: (?:.*?);(.*?)?\n(.*)/m =~ tmp then
