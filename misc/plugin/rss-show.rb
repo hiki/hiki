9902dd39c33baa921273b66eb44669faef9b9181
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-09-26 13:35:05 +0000

    * misc/plugin/rss-show.rb: use the rss library.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@779 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- misc/plugin/rss-show.rb
+++ misc/plugin/rss-show.rb
@@ -1,11 +1,12 @@
-# $Id: rss-show.rb,v 1.10 2005-06-27 07:46:44 fdiary Exp $
+# $Id: rss-show.rb,v 1.11 2005-09-26 13:35:05 fdiary Exp $
 # Copyright (C) 2003 TAKEUCHI Hitoshi <hitoshi@namaraii.com>
 
-require 'cgi'
+require 'rss/1.0'
+require 'rss/2.0'
 
 def rss_show(url, cache_time = 1800, number = 5)
   if rss = rss_get(url.untaint, cache_time)
-    items = rss_parse(rss)
+    items = RSS::Parser.parse(rss, false).items
     rss_format_items(items[0...number])
   else
     ''
@@ -14,7 +15,7 @@ end
 
 def rss_get(url, cache_time)
   Dir.mkdir("#{@cache_path}/rss-show") unless File.exist?("#{@cache_path}/rss-show")
-  cache_file = "#{@cache_path}/rss-show/#{CGI::escape(url)}".untaint
+  cache_file = "#{@cache_path}/rss-show/#{url.escape}".untaint
 
   begin
     rss_recent_cache(url, cache_file, cache_time)
@@ -56,16 +57,12 @@ def rss_recent_cache(url, cache_file, cache_time)
   end
 end
 
-def rss_parse(rss)
-  rss_re = /<item(?!s)[^>]*?>.*?<title[^>]*?>(.*?)<\/title>.*?<link[^>]*?>(.*?)<\/link>.*?<\/item>/mi
-  utf8_to_euc(rss).scan(rss_re)
-end
-
 def rss_format_items(items)
   html = "<ul>\n"
   items.each do |i|
-    page, url = i
-    html << %Q!<li>! + make_anchor("#{url}", "#{page.escapeHTML}") + %Q!</li>\n!
+    page = utf8_to_euc( i.title )
+    url = utf8_to_euc( i.link )
+    html << "<li>#{make_anchor(url.escapeHTML, page.unescapeHTML.escapeHTML)}</li>\n"
   end
   html << "</ul>\n"
 end
