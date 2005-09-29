28de6cfeff13b1def09d94e83054a228f26c3138
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-09-29 02:48:21 +0000

    * hiki/xmlrpc.rb: use utf-8 as the input/output charset. 	add 'wiki.getPageInfo'.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@785 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- hiki/xmlrpc.rb
+++ hiki/xmlrpc.rb
@@ -1,4 +1,4 @@
-# $Id: xmlrpc.rb,v 1.4 2005-08-24 06:43:04 fdiary Exp $
+# $Id: xmlrpc.rb,v 1.5 2005-09-29 02:48:21 fdiary Exp $
 
 require 'xmlrpc/server'
 require 'hiki/plugin'
@@ -22,10 +22,31 @@ module Hiki
     private
     def init_handler
       @server.add_handler('wiki.getPage') do |page|
+        page = utf8_to_euc( page )
         conf = Hiki::Config::new
         db = Hiki::HikiDB::new( conf )
         begin
-          ret = XMLRPC::Base64.new( db.load( page ) || '' )
+          ret = XMLRPC::Base64.new( euc_to_utf8( db.load( page ) || '' ) )
+        rescue
+          STDERR.puts $!, $@.inspect
+          ret = false
+        end
+        ret
+      end
+
+      @server.add_handler('wiki.getPageInfo') do |page|
+        page = utf8_to_euc( page )
+        conf = Hiki::Config::new
+        db = Hiki::HikiDB::new( conf )
+        begin
+          title = db.get_attribute( page, :title )
+          title = page if title.nil? || title.empty?
+          ret = {
+            'title' => XMLRPC::Base64.new( euc_to_utf8( title ) ),
+            'keyword' => db.get_attribute( page, :keyword ).collect { |k| XMLRPC::Base64.new( euc_to_utf8( k ) ) },
+            'lastModified' => db.get_attribute( page, :last_modified ),
+            'author' => XMLRPC::Base64.new( db.get_attribute( page, :editor ) || '' )
+          }
         rescue
           STDERR.puts $!, $@.inspect
           ret = false
@@ -34,7 +55,10 @@ module Hiki
       end
 
       @server.add_handler('wiki.putPage') do |page, content, attributes|
+        page = utf8_to_euc( page )
+        content = utf8_to_euc( content )
         attributes ||= {}
+        attributes.each_pair { |k, v| v.replace( utf8_to_euc( v ) ) }
         conf = Hiki::Config::new
         cgi = CGI::new
         cgi.params['c'] = ['save']
@@ -52,7 +76,7 @@ module Hiki
           raise "can't edit this page." unless plugin.editable?( page )
 
           md5hex = db.md5hex( page )
-          plugin.save( page, content.gsub(/\r/, ''), md5hex )
+          plugin.save( page, content.gsub( /\r/, '' ), md5hex )
           keyword = attributes['keyword'] || []
           title = attributes['title']
           attr = [[:keyword, keyword.uniq], [:editor, plugin.user]]
@@ -73,7 +97,7 @@ module Hiki
         conf = Hiki::Config::new
         db = Hiki::HikiDB::new( conf )
         begin
-          ret = db.pages.collect{|p| XMLRPC::Base64.new( p )}
+          ret = db.pages.collect{|p| XMLRPC::Base64.new( euc_to_utf8( p ) )}
         rescue
           STDERR.puts $!, $@.inspect
           ret = false
