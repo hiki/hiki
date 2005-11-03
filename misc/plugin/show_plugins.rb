e04d5d5d973dc72ea4c2dc86dbd13d34aefedad0
yanagita <yanagita@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2005-11-03 07:50:41 +0000

    new plugin. It displays enabled plugins.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@803 684b846f-27f2-41ea-b66a-77a1bafa84c1

---
+++ misc/plugin/show_plugins.rb
@@ -0,0 +1,5 @@
+# $Id: show_plugins.rb,v 1.1 2005-11-03 07:50:40 yanagita Exp $
+# Copyright (C) 2005 Kouhei Yanagita <yanagi@shakenbu.org>
+
+export_plugin_methods(:show_plugins)
+
--- plugin/01sp.rb
+++ plugin/01sp.rb
@@ -1,4 +1,4 @@
-# 01sp.rb - select-plugins plugin $Revision: 1.3 $
+# 01sp.rb - select-plugins plugin $Revision: 1.4 $
 
 =begin ChangeLog
 See ../ChangeLog for changes after this.
@@ -36,138 +36,151 @@ See ../ChangeLog for changes after this.
 SP_PREFIX = 'sp'
 @sp_path = ( @conf["#{SP_PREFIX}.path"] || "#{PATH}/misc/plugin" ).to_a
 @sp_path = @sp_path.collect do |path|
-        /\/$/ =~ path ? path.chop : path
+  /\/$/ =~ path ? path.chop : path
 end
 
 # get plugin option
 def sp_option( key )
-        @conf["#{SP_PREFIX}.#{key}"]
+  @conf["#{SP_PREFIX}.#{key}"]
 end
 
 # hash of paths from array of dirs
 def sp_hash_from_dirs( dirs )
-        r = Hash.new
-        dirs.each do |dir|
-                Dir::glob( "#{dir}/*.rb" ).each do |path|
-                        filename = File.basename( path )
-                        unless r[ filename ] then
-                                r[ filename ] = path
-                        end
-                end
-        end
-        r
+  r = Hash.new
+  dirs.each do |dir|
+    Dir::glob( "#{dir}/*.rb" ).each do |path|
+      filename = File.basename( path )
+      r[filename] ||= path
+    end
+  end
+  r
 end
 
 # url of the document
 def sp_doc_url( file )
-	case @conf.lang
-	when 'ja'
-	        "http://hikiwiki.org/ja/#{CGI::escape( file )}.html"
-	else
-	        "http://hikiwiki.org/en/#{CGI::escape( file )}.html"
-	end
+  case @conf.lang
+  when 'ja'
+    "http://hikiwiki.org/ja/#{CGI::escape( file )}.html"
+  else
+    "http://hikiwiki.org/en/#{CGI::escape( file )}.html"
+  end
+end
+
+def collect_plugins( sp_opt )
+  # categorize the available plugins
+  used = []
+  notused = []
+  unknown = []
+  # File.basenmame needed to read option from 01sp.rb <= 1.10
+  selected_array = sp_option( 'selected' ) ? sp_option( 'selected' ).split( /\n/ ).collect{ |p| File.basename( p ) } : []
+  notselected_array = sp_option( 'notselected' ) ? sp_option( 'notselected').split( /\n/ ).collect{ |p| File.basename( p ) } : []
+  sp_opt.keys.each do |path|
+    if selected_array.include?( path ) then
+      used << path
+    elsif notselected_array.include?( path ) then
+      notused << path
+    else
+      unknown << path
+    end
+  end
+  [used, notused, unknown]
 end
 
 # <li> list of plugins
-def sp_li_plugins( paths, is_checked )
-        r = ''
-        paths.collect { |path| File.basename( path ) }.sort.each do |file|
-                r += <<-_HTML
-                        <li><input name="#{SP_PREFIX}.#{CGI::escapeHTML( file )}" type="checkbox" value="t"#{is_checked ? ' checked' : ''}><a href="#{sp_doc_url( file )}">#{CGI::escapeHTML( file )}</a>
-                _HTML
-        end
-        r
+def sp_li_plugins( paths, with_checkbox, is_checked )
+  paths.collect { |path| File.basename( path ) }.sort.inject('') do |result, file|
+    checkbox = with_checkbox ? %Q!<input name="#{SP_PREFIX}.#{CGI::escapeHTML( file )}" type="checkbox" value="t"#{is_checked ? ' checked' : ''}>! : ''
+    result << %Q!<li>#{checkbox}<a href="#{sp_doc_url( file )}">#{CGI::escapeHTML( file )}</a>!
+  end
 end
 
 # lists of plugins
-def sp_list_plugins( sp_opt )
-        r = ''
-        unless sp_opt.empty? then
-                # categorize the available plugins
-                used = Array.new
-                notused = Array.new
-                unknown = Array.new
-                # File.basenmame needed to read option from 01sp.rb <= 1.10
-                selected_array = sp_option( 'selected' ) ? sp_option( 'selected').split( /\n/ ).collect{ |p| File.basename( p ) } : []
-                notselected_array = sp_option( 'notselected' ) ? sp_option( 'notselected').split( /\n/ ).collect{ |p| File.basename( p ) } : []
-                sp_opt.keys.each do |path|
-                        if selected_array.include?( path ) then
-                                used << path
-                        elsif notselected_array.include?( path ) then
-                                notused << path
-                        else
-                                unknown << path
-                        end
-                end
+def sp_list_plugins( sp_opt, with_checkbox )
+  r = ''
+  if sp_opt.empty?
+    return @sp_label_noplugin
+  else
+    used, notused, unknown = collect_plugins( sp_opt )
 
-                # list up
-                r += @sp_label_please_select
-                unless unknown.empty? then
-                        r += @sp_label_new
-                        r += "<ul>\n" 
-                        r += sp_li_plugins( unknown, sp_option( 'usenew' ) )
-                        r += "</ul>\n"
-                end
-                # selected plugins
-                unless used.empty? then
-                        r += @sp_label_used
-                        r += "<ul>\n"
-                        r += sp_li_plugins( used, true )
-                        r += "</ul>\n"
-                end
-                # not selected plugins
-                unless notused.empty? then
-                        r += @sp_label_notused
-                        r += "<ul>\n"
-                        r += sp_li_plugins( notused, false )
-                        r += "</ul>\n"
-                end
-        else
-                r += @sp_label_noplugin
-        end
-        r
+    # list up
+    r << @sp_label_please_select
+    unless unknown.empty? then
+      r << @sp_label_new
+      r << "<ul>\n" 
+      r << sp_li_plugins( unknown, with_checkbox, sp_option( 'usenew' ) )
+      r << "</ul>\n"
+    end
+
+    # selected plugins
+    unless used.empty? then
+      r << @sp_label_used
+      r << "<ul>\n"
+      r << sp_li_plugins( used, with_checkbox, true )
+      r << "</ul>\n"
+    end
+
+    # not selected plugins
+    unless notused.empty? then
+      r << @sp_label_notused
+      r << "<ul>\n"
+      r << sp_li_plugins( notused, with_checkbox, false )
+      r << "</ul>\n"
+    end
+  end
+  r
 end
 
 # things needed to configure this plugin
-if SP_PREFIX == @cgi.params['conf'][0] then
-        # list of plugins
-        @sp_opt = sp_hash_from_dirs( @sp_path )
+if SP_PREFIX == @cgi.params['conf'][0]
+  # list of plugins
+  @sp_opt = sp_hash_from_dirs( @sp_path )
 
-        # update options
-        # we have to do this when we are eval'ed to update the config menu
-        if /saveconf/ =~ @mode then
-                @conf["#{SP_PREFIX}.selected"] = ''
-                @conf["#{SP_PREFIX}.notselected"] = ''
-                @sp_opt.each_key do |file|
-                        if 't' == @cgi.params["#{SP_PREFIX}.#{file}"][0] then
-                                @conf["#{SP_PREFIX}.selected"] << "#{file}\n"
-                        else
-                                @conf["#{SP_PREFIX}.notselected"] << "#{file}\n"
-                        end
-                end
-        end
+  # update options
+  # we have to do this when we are eval'ed to update the config menu
+  if /saveconf/ =~ @mode
+    @conf["#{SP_PREFIX}.selected"] = ''
+    @conf["#{SP_PREFIX}.notselected"] = ''
+    @sp_opt.each_key do |file|
+      if 't' == @cgi.params["#{SP_PREFIX}.#{file}"][0]
+        @conf["#{SP_PREFIX}.selected"] << "#{file}\n"
+      else
+        @conf["#{SP_PREFIX}.notselected"] << "#{file}\n"
+      end
+    end
+  end
 end
 
 # configuration menu
 # options are updated when we are eval'ed
 add_conf_proc( SP_PREFIX, @sp_label ) do
-        r = @sp_label_description + sp_list_plugins( @sp_opt )
+  r = ''
+  r << @sp_label_description
+  r << sp_list_plugins( @sp_opt, true )
 end
 
 # Finally, we can eval the selected plugins as tdiary.rb does
-if sp_option( 'selected' ) then
-        sp_option( 'selected' ).untaint.split( /\n/ ).collect{ |p| File.basename( p ) }.sort.each do |filename|
-                @sp_path.each do |dir|
-                        path = "#{dir}/#{filename}"
-                        if File.readable?( path ) then
-                                begin
-                                        load_plugin( path )
-                                        @plugin_files << path
-                                rescue Exception
-                                        raise PluginError::new( "Plugin error in '#{path}'.\n#{$!}" )
-                                end
-                                break
-                        end
-                end
+if sp_option( 'selected' )
+  sp_option( 'selected' ).untaint.split( /\n/ ).collect{ |p| File.basename( p ) }.sort.each do |filename|
+    @sp_path.each do |dir|
+      path = "#{dir}/#{filename}"
+      if File.readable?( path )
+        begin
+          load_plugin( path )
+          @plugin_files << path
+        rescue Exception
+          raise PluginError::new( "Plugin error in '#{path}'.\n#{$!}" )
         end
+        break
+      end
+    end
+  end
 end
+
+# The `show_plugins' plugin is enabled in show_plugins.rb
+def show_plugins
+  used = @conf["#{SP_PREFIX}.selected"].split( /\n/ ).collect{ |p| File.basename( p ) }.sort
+  used.empty? ? '' : "<ul>\n" + sp_li_plugins( used, false, true ) + "</ul>\n"
+end
+
+export_plugin_methods()
+
