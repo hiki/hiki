93e0ae04791982445ebdfb557d0468f92a28d44f
yanagita <yanagita@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2006-02-18 06:00:35 +0000

    add a comment.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@839 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- misc/plugin/show_plugins.rb
+++ misc/plugin/show_plugins.rb
@@ -1,5 +1,7 @@
-# $Id: show_plugins.rb,v 1.1 2005-11-03 07:50:40 yanagita Exp $
+# $Id: show_plugins.rb,v 1.2 2006-02-18 06:00:35 yanagita Exp $
 # Copyright (C) 2005 Kouhei Yanagita <yanagi@shakenbu.org>
+
+# implemented in 01sp.rb
 
 export_plugin_methods(:show_plugins)
 
