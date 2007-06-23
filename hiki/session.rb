ac41641128259317aa18107d4073336fded88af2
yanagita <yanagita@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2007-06-23 13:32:25 +0000

    * hiki/session.rb (create_new_id): use Time#usec to make a session id more hard-to-crack.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@893 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- hiki/session.rb
+++ hiki/session.rb
@@ -1,4 +1,4 @@
-# $Id: session.rb,v 1.9 2006-08-07 01:57:11 fdiary Exp $
+# $Id: session.rb,v 1.10 2007-06-23 13:32:25 yanagita Exp $
 # Copyright (C) 2004 Kazuhiko <kazuhiko@fdiary.net>
 
 module Hiki
@@ -76,7 +76,9 @@ module Hiki
     def create_new_id
       require 'digest/md5'
       md5 = Digest::MD5::new
-      md5.update(String(Time::now))
+      now = Time::now
+      md5.update(now.to_s)
+      md5.update(String(now.usec))
       md5.update(String(rand(0)))
       md5.update(String($$))
       md5.update('foobar')
