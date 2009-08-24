49d6e65c2586ee0dc6fa587c42f24128db1f4f71
fdiary <fdiary@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2009-08-24 05:53:26 +0000

    * style/default/parser.rb, style/default/html_formatter.rb: 	replacing WikiName links is now handled by HikiDoc by specifying 	use_wiki_name parameter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/trunk@1006 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- style/default/parser.rb
+++ style/default/parser.rb
@@ -21,10 +21,12 @@ module Hiki
     end
     
     def initialize( conf )
+      @use_wiki_name = conf.use_wikiname
     end
 
     def parse( s, top_level = 2 )
-      HikiDoc.to_html( s, :level => top_level )
+      HikiDoc.to_html( s, :level => top_level,
+                       :use_wiki_name => @use_wiki_name )
     end
   end
 end
