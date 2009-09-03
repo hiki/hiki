95ca20e03d20cac3a64fed56f57d4e71e54f8b24
okkez <okkez@684b846f-27f2-41ea-b66a-77a1bafa84c1>
2009-09-03 14:04:15 +0000

    cannot store any object excluding string in ENV.
    a patch from Tomohiro Nishimura <tomohiro68 AT gmail.com>
    
    git-svn-id: http://svn.sourceforge.jp/svnroot/hiki/hiki/branches/rack@1043 684b846f-27f2-41ea-b66a-77a1bafa84c1

--- hiki/app.rb
+++ hiki/app.rb
@@ -13,7 +13,7 @@ module Hiki
       request = Rack::Request.new(env)
       # TODO use Rack::Request#env or other methods instead of ENV
       # HACK replace ENV values to web application environment
-      env.each{|k,v| ENV[k] = v unless /\Arack\./ =~ k }
+      env.each{|k,v| ENV[k] = v.to_s unless /\Arack\./ =~ k }
       conf = Hiki::Config.new
       response = nil
       if %r|text/xml| =~ request.content_type and request.post?
