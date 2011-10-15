7dfc13ee40c5b6ff0c4cb6689fa5feb18b1c93b1
okkez <okkez000@gmail.com>
2011-10-15 23:18:22 +0900

    use test-unit Gem.

--- test/runner.rb
+++ test/runner.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
+gem 'test-unit'
 require 'test/unit'
+require 'test/unit/notify'
 
-rootdir = "#{File::dirname($0)}/.."
-$:.unshift( rootdir, "#{rootdir}/hiki" )
+rootdir = Pathname(__FILE__).dirname.parent.expand_path
+$:.unshift(rootdir, "#{rootdir}/hiki")
 
 exit Test::Unit::AutoRunner.run(true, File.dirname($0))
