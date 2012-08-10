eb7179cc0dfb2ceba81d4b8a38edc22bcc5f773b
okkez <okkez000@gmail.com>
2012-08-10 23:42:00 +0900

    Fix coding style

--- test/test_plugin_rss.rb
+++ test/test_plugin_rss.rb
@@ -12,23 +12,29 @@ class Plugin_RSS_Unit_Tests < Test::Unit::TestCase
     @now = Time.parse(CGI.rfc1123_date(Time.now))
     @request = Object.new
     class << @request
-      def params; {}; end
+      def params
+        {}
+      end
     end
     @conf = Object.new
     class << @conf
-      def charset; end
-      def lang; end
+      def charset
+      end
+      def lang
+      end
     end
     plugin_file = File.expand_path(File.join(File.dirname(__FILE__), *%w{.. misc plugin rss.rb}))
     instance_eval(File.read(plugin_file))
     class << self
-      define_method(:rss_body) {|*page_num| ['', @now]}
+      define_method(:rss_body) {|*page_num|
+        ['', @now]
+      }
     end
   end
 
   def test_rss_returns_304_when_if_modified_since_is_same_to_last_modified
     ENV['HTTP_IF_MODIFIED_SINCE'] = CGI.rfc1123_date(@now)
-    assert_equal 304, rss.status
+    assert_equal(304, rss.status)
   end
 
   def add_body_enter_proc(prcedure)
