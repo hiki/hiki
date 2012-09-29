ec1e25b1099909bfd25b0b9eac1a63f92266614b
okkez <okkez000@gmail.com>
2012-09-29 12:33:55 +0900

    Use real files

--- test/test_flatfile.rb
+++ test/test_flatfile.rb
@@ -1,43 +1,28 @@
 # coding: utf-8
 
 require 'test/unit'
+require 'hiki/config'
 require 'hiki/db/flatfile'
 require 'fileutils'
-require 'tempfile'
-
+require 'test_helper'
 
 class HikiDB_flatfile_Unit_Tests < Test::Unit::TestCase
+  include TestHelper
+  include FileUtils
+
   def setup
-    @db = Hiki::HikiDB_flatfile.new(stub_conf)
+    @wiki_data_path = fixtures_dir + "plain_data"
+    cp_r(fixtures_dir + "plain_data.prepare", @wiki_data_path)
+    config_path = (fixtures_dir + "hikiconf_default.rb").expand_path
+    @conf = Hiki::Config.new(config_path)
+    @db = Hiki::HikiDB_flatfile.new(@conf)
   end
 
   def teardown
-    FileUtils.rm_r(test_data_path) if File.exists?(test_data_path)
+    rm_rf(@wiki_data_path)
   end
 
   def test_close_db
     assert(@db.close_db)
-  end
-
-  def stub_conf
-    if ! defined?(@stub_conf)
-      @stub_conf = Object.new
-      @stub_conf.instance_eval(%{def data_path; "#{test_data_path}"; end})
-    end
-    @stub_conf
-  end
-
-  def test_data_path
-    @test_data_path ||= File.join(tempdir, self.class.to_s)
-  end
-
-  def tempdir
-    if ! defined?(@tempdir)
-      tempfile = Tempfile.new(self.class.to_s + Time.now.to_i.to_s)
-      @tempdir = File.dirname(tempfile.path)
-      FileUtils.mkdir(@tempdir) unless File.exists?(@tempdir)
-      tempfile.close(true)
-    end
-    @tempdir
   end
 end
