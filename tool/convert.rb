857216caf1da95e02121e3a8c5aa85f95f9e7d1d
okkez <okkez000@gmail.com>
2012-09-30 00:01:41 +0900

    Work for flatfile database

--- tool/convert.rb
+++ tool/convert.rb
@@ -4,33 +4,49 @@ $LOAD_PATH.unshift '.'
 
 require "optparse"
 require "pathname"
+require "fileutils"
+require "digest/md5"
 require "hiki/util"
+require "hiki/config"
 
-def convert(data_dir, repository_class, input_encoding, output_encoding)
-  repository = repository_class.new(nil, data_dir)
-  repository.pages do |page|
-    new_page = page.encode!(output_encoding, input_encoding)
-    repository.rename(page, new_page)
+def convert(data_path, database_class, input_encoding, output_encoding)
+  config = Struct.new(:data_path).new
+  config.data_path = data_path.expand_path
+  db = database_class.new(config)
+  db.pages.each do |page|
+    old_page = page
+    new_page = old_page.dup.encode!(output_encoding, input_encoding)
+    old_text = db.load(old_page)
+    new_text = old_text.dup.encode!(output_encoding, input_encoding)
+    if old_page == new_page
+      db.unlink(old_page)
+    else
+      db.rename(old_page, new_page)
+    end
+    db.store(new_page, new_text, Digest::MD5.hexdigest(old_text))
   end
-  repository.pages do |page|
-    latest_revision = repository.revisions(page).first.last
-    content = repository.get_revision(page, latest_revision)
-    repository.commit_with_content(page, content.encode!(output_encoding, input_encoding))
-  end
+  cache_path = data_path + "cache"
+  FileUtils.rm_rf(cache_path)
 end
 
 def main(argv)
   parser = OptionParser.new
-  data_dir = nil
+  data_path = nil
   repository_type = "plain"
+  database_type = "flatfile"
   input_encoding = nil
   output_encoding = nil
-  parser.on("-d", "--data-directory=DIR", "Specify the data directory"){|dir|
-    data_dir = dir
+  parser.on("-D", "--data-directory=DIR", "Specify the data directory"){|dir|
+    data_path = Pathname(dir).realpath
   }
-  parser.on("-r", "--repository-type=[TYPE]",
-            "Specify the repository type [plain, svn, git, hg] (default: plain") {|type|
-    repository_type = type
+  # TODO Do we need to handle repository type?
+  # parser.on("-r", "--repository-type=[TYPE]",
+  #           "Specify the repository type [plain, svn, git, hg] (default: plain") {|type|
+  #   repository_type = type
+  # }
+  parser.on("-d", "--database-type=[TYPE]",
+            "Specify the database type [flatfile] (default: flatfile") {|type|
+    database_type = type
   }
   parser.on("-i", "--input-encoding=ENCODING", "Specify the input encoding"){|encoding|
     input_encoding = Encoding.find(encoding)
@@ -47,10 +63,12 @@ def main(argv)
     exit 1
   end
 
-  require_relative "../hiki/repos/#{repository_type}"
-  repository_class = ::Hiki.const_get("Repos#{repository_type.capitalize}")
+  # require_relative "../hiki/repos/#{repository_type}"
+  # repository_class = ::Hiki.const_get("Repos#{repository_type.capitalize}")
+  require_relative "../hiki/db/#{database_type}"
+  database_class = ::Hiki::const_get("HikiDB_#{database_type}")
 
-  convert(data_dir, repository_class, input_encoding, output_encoding)
+  convert(data_path, database_class, input_encoding, output_encoding)
 end
 
 if __FILE__ == $0
