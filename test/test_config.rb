b82ee84005602af649f7c251aa4d25da933e18e4
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-05-12 09:35:01 +0900

    move config file to fixtures directory

--- test/test_config.rb
+++ test/test_config.rb
@@ -6,7 +6,7 @@ require 'hiki/config'
 class Config_Unit_Tests < Test::Unit::TestCase
   def setup
     base_dir = File.dirname(__FILE__)
-    @config_path = "#{base_dir}/hikiconf_test.rb"
+    @config_path = "#{base_dir}/fixtures/hikiconf_test.rb"
   end
 
   def test_initialize
