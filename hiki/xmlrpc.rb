e6b8d281db4a0aeb7e06dd0df4c1bb1ecedd7ef9
okkez <okkez000@gmail.com>
2014-07-03 15:20:21 +0900

    Fix style

--- hiki/xmlrpc.rb
+++ hiki/xmlrpc.rb
@@ -10,34 +10,34 @@ module Hiki
     def init_handler(server, conf, request)
       server.add_handler('wiki.getPage') do |page|
         db = conf.database
-        ret = db.load( page )
+        ret = db.load(page)
         unless ret
           raise XMLRPC::FaultException.new(1, "No such page was found.")
         end
-        XMLRPC::Base64.new( ret )
+        XMLRPC::Base64.new(ret)
       end
 
       server.add_handler('wiki.getPageInfo') do |page|
         db = conf.database
-        title = db.get_attribute( page, :title )
+        title = db.get_attribute(page, :title)
         title = page if title.nil? || title.empty?
         {
-          'title' => XMLRPC::Base64.new( title ),
-          'keyword' => db.get_attribute( page, :keyword ).collect { |k| XMLRPC::Base64.new(  k ) },
-          'md5hex' => db.md5hex( page ),
-          'lastModified' => db.get_attribute( page, :last_modified ).getutc,
-          'author' => XMLRPC::Base64.new( db.get_attribute( page, :editor ) || '' )
+          'title' => XMLRPC::Base64.new(title),
+          'keyword' => db.get_attribute(page, :keyword).collect {|k| XMLRPC::Base64.new(  k ) },
+          'md5hex' => db.md5hex(page),
+          'lastModified' => db.get_attribute(page, :last_modified).getutc,
+          'author' => XMLRPC::Base64.new(db.get_attribute(page, :editor) || '')
         }
       end
 
       server.add_handler('wiki.putPage') do |page, content, attributes|
         attributes ||= {}
-        attributes.each_pair { |k, v|
+        attributes.each_pair {|k, v|
           case v
           when String
-            v.replace( v )
+            v.replace(v)
           when Array
-            v.map!{ |s| s.replace( s ) }
+            v.map!{|s| s.replace(s) }
           end
         }
         request.params['c'] = 'save'
@@ -49,33 +49,33 @@ module Hiki
         options['cgi']      = request # for backward compatibility
         options['db']       = db
         options['params']   = Hash.new('')
-        plugin = Hiki::Plugin.new( options, conf )
-        plugin.login( attributes['name'], attributes['password'] )
+        plugin = Hiki::Plugin.new(options, conf)
+        plugin.login(attributes['name'], attributes['password'])
         Hiki::Filter.init(conf, request, plugin, db)
 
-        unless plugin.editable?( page )
+        unless plugin.editable?(page)
           raise XMLRPC::FaultException.new(10, "can't edit this page.")
         end
 
         md5hex = attributes['md5hex'] || db.md5hex( page )
         update_timestamp = !attributes['minoredit']
-        unless plugin.save( page, content.gsub( /\r/, '' ), md5hex, update_timestamp )
+        unless plugin.save(page, content.gsub( /\r/, '' ), md5hex, update_timestamp)
           raise XMLRPC::FaultException.new(11, "save failed.")
         end
-        keyword = attributes['keyword'] || db.get_attribute( page, :keyword )
+        keyword = attributes['keyword'] || db.get_attribute(page, :keyword)
         title = attributes['title']
         attr = [[:keyword, keyword.uniq], [:editor, plugin.user]]
         attr << [:title, title] if title
         db.set_attribute(page, attr)
-        if plugin.admin? && attributes.has_key?( 'freeze' )
-          db.freeze_page( page, attributes['freeze'] ? true : false)
+        if plugin.admin? && attributes.has_key?('freeze')
+          db.freeze_page(page, attributes['freeze'] ? true : false)
         end
         true
       end
 
       server.add_handler('wiki.getAllPages') do
         db = conf.database
-        db.pages.collect{|p| XMLRPC::Base64.new( p )}
+        db.pages.collect{|p| XMLRPC::Base64.new(p)}
       end
 
       #add_multicall
