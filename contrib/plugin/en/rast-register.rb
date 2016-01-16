3f42ce87bd0f78e337570896c2a0fbc962ddc5c4
okkez <okkez000@gmail.com>
2016-01-16 22:39:03 +0900

    Remove outdated files

--- contrib/plugin/en/rast-register.rb
+++
@@ -1,3 +0,0 @@
-@rast_register_conf_label = 'Rast Search'
-@rast_register_conf_header = 'Rebuild Rast search index'
-@rast_register_conf_description = 'To rebuild Rast search index, check the box and submit \'OK\'.'
--- contrib/plugin/ja/rast-register.rb
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-@rast_register_conf_label = 'Rast検索'
-@rast_register_conf_header = 'Rast検索インデックスの再構築'
-@rast_register_conf_description = 'Rast検索のインデックスを再構築する場合は、チェックボックスをチェックしてOKを押してください。'
