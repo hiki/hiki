9c560eefd17ed2b7c8921d98c7ccbee1d4db6c8d
okkez <okkez000@gmail.com>
2016-01-19 00:00:37 +0900

    Add style.rb

---
+++ lib/hiki/style.rb
@@ -0,0 +1,11 @@
+module Hiki
+  module Style
+  end
+end
+
+require 'hiki/style/default/parser'
+require 'hiki/style/default/formatter'
+require 'hiki/style/rd+/parser'
+require 'hiki/style/rd+/formatter'
+require 'hiki/style/math/parser'
+require 'hiki/style/math/formatter'
